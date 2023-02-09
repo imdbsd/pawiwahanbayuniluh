57536721500e6f27926fa43e0d5392c8d7abf4b7
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2023-02-09 11:04:18 +0700

    Update env.php

--- env.php
+++ env.php
@@ -1,12 +1,12 @@
 <?php
-    $envStatus = "development";    
+    $envStatus = "production";    
     $env = array(
         "site_name" => "Pawiwahan Bayu & Niluh",
         "development" => array(
             "site_url" => "http://localhost/pawiwahanbayuniluh"
         ),
         "production" => array(
-            "site_url" => "https://pawiwahanbayuniluh.com"
+            "site_url" => "https://imdbsd.github.io/pawiwahanbayuniluh/"
         )
     );    
 ?>
